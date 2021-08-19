7541e8097551ef351dd5c26c922738ce468e24c5
VasilisMos <64889653+VasilisMos@users.noreply.github.com>
2021-08-19 19:42:51 +0300

    Update timing_performance.m
    Now working with on MATLAB. 
    TODO: On Octave the file IO is not working, fix with try/catch if possible.

--- matlab/timing_performance.m
+++ matlab/timing_performance.m
@@ -1,28 +1,35 @@
-n = (1:10) * 1e6;
-
-for i=1:length(n)
-    fprintf("Iteration %d/%d, n=%d\n",i,length(n),n(i))
-    for k=1:5
-        feval("generateDatasets",n(i));
-    end
-end
-
-
-
-function mat_benchmark()
+%% timing_performance.m
+% This function evaluates the time performance of 
+% the MATLAB version of Sparse Boolean Matrix Multiplication
+% (which performs normal sparse matrix multiplication followed by
+% a value thresholding)
+function timing_performance()
     d = 2;
+    max_iters=13;
     t = [];
-    sizes = 1e3:5e3:5e5; % 1e3:5e3:5e6;
-    waitbar(0,"Matlab boolean matrix multiplication benchmark..");
+    sizes = 1e5:3e5:5e6;
+
+    h = waitbar(0,"Matlab boolean matrix multiplication benchmark..");
     for n = sizes
-      A = sprand( n, n, d/n) > 0;
-      B = sprand( n, n, d/n) > 0;
-      tic; C = (A*B) > 0; t1 = toc; t = [t t1];
-      waitbar(n/max(sizes));
+        t1 = [];
+        for iters = 1:max_iters
+            A = sprand( n, n, d/n) > 0;
+            B = sprand( n, n, d/n) > 0;
+            tic; C = (A*B) > 0; temp = toc; t1 = [t1 temp];
+        end
+
+        t = [t median(t1)];
+        waitbar(n/max(sizes));
     end
     
     figure;
     plot(sizes,t);
     title("Boolean Matrix Multiplication - MATLAB Implementation");
     xlabel("N"); ylabel("Time (sec)");
+    close(h); pause; close all;
+
+    fileID = fopen('MATLAB_times.txt','w');
+    fprintf(fileID,'%s,%s\n','dim','times');
+    fprintf(fileID,'%d,%.4f\n',[sizes; t]);
+    fclose(fileID);
 end
